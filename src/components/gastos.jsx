b0fdbf53c3e293c699b35cb7dd3826bbd8465e58
thomasHeitFux <thomas41392@gmail.com>
2023-03-05 19:50:18 -0300

    refactoring

--- src/components/gastos.jsx
+++ src/components/gastos.jsx
@@ -37,42 +37,37 @@ export const Gastos = () => {
     return (
         <div className="p-8">
             <button onClick={(e) => handleExport(e)}>Export</button>
-            <div className="m-3 uppercase text-black font-bold flex p-2 justify-between"> <h1>tipo</h1><h1>estructura</h1><h1>importe</h1><h1>detalle</h1><h1>metodo</h1><h1>responsable</h1><h1>fecha</h1><button  className="bg-gray-200 text-gray-200 font-bold py-2 px-4 rounded">X</button></div>
+            <div className="m-3 uppercase text-black font-bold flex p-2 justify-between"> <h1>tipo</h1><h1>estructura</h1><h1>importe</h1><h1>detalle</h1><h1>metodo</h1><h1>responsable</h1><h1>fecha</h1><button className="bg-gray-200 text-gray-200 font-bold py-2 px-4 rounded">X</button></div>
             <table className="w-full" id="tabla">
 
-                {gastos.map((e) => {
+                {gastos.gastos.map((e) => {
 
 
                     return (<tr key={e.id} className="m-3 bg-gray-100 flex p-2 justify-between">
 
 
                         <div className="">
-                            {/* <h1 className="font-bold uppercase">Tipo:</h1> */}
                             <h1>{e.tipo}</h1>
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">Estructura:</h1> */}
                             <h1>{e.estructura}</h1>
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">importe:</h1> */}
+
                             <h1>{e.importe}€</h1>
 
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">detalle:</h1> */}
+
                             <h1>{e.detalles}</h1>
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">metodo:</h1> */}
                             <h1>{e.metodo}</h1>
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">responsable:</h1> */}
                             <h1>{e.responsable}</h1>
                         </div>
                         <div>
-                            {/* <h1 className="font-bold uppercase">Fecha:</h1> */}
                             <h1>{e.fecha}</h1>
                         </div>
                         <button onClick={() => show(e.id)} className="bg-red-500 duration-300 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">X</button>
