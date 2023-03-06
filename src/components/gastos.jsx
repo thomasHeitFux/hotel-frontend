c5da6c13d1774207f5dff0a821f62a8ad9045246
thomasHeitFux <thomas41392@gmail.com>
2023-03-06 11:37:22 -0300

    add style button export

--- src/components/gastos.jsx
+++ src/components/gastos.jsx
@@ -39,7 +39,7 @@ export const Gastos = () => {
     }
     return (
         <div className="p-8">
-            <button onClick={(e) => handleExport(e)}>Export</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(e) => handleExport(e)}>Export</button>
             <div className="m-3 uppercase text-black font-bold flex p-2 justify-between"> <h1>tipo</h1><h1>estructura</h1><h1>importe</h1><h1>detalle</h1><h1>metodo</h1><h1>responsable</h1><h1>fecha</h1><button className="bg-gray-200 text-gray-200 font-bold py-2 px-4 rounded">X</button></div>
             <table className="w-full" id="tabla">
 
